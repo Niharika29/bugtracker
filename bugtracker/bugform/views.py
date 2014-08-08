2fea2d1999346b1996ac6c7bfb3db3308bdb0c02
Niharika Kohli <niharikakohli29@gmail.com>
2014-08-08 07:18:44 +0000

    Undo IPware

--- bugtracker/bugform/views.py
+++ bugtracker/bugform/views.py
@@ -6,7 +6,7 @@ from bugform.forms import BugForm, AdminForm, SimpleTable
 from bugform.models import BugModel, AdminModel
 import django_tables2 as tables 
 from django_tables2 import RequestConfig
-from ipware.ip import get_ip
+#from ipware.ip import get_ip
 
 def admin(request):
 	if request.method == 'POST':
@@ -51,7 +51,7 @@ def index(request):
 			return render(request, template, context)
 
 	else:
-		ip = get_ip(request)
+		ip = getip(request)
 		geocity = pygeoip.GeoIP('GeoLiteCity.dat')
 		city = geocity.record_by_addr('122.161.236.2')
 		data = {'ip':ip, 
