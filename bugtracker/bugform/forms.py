949148f813ef2f3fe4174470f4d00034ebabd2f9
Niharika29 <niharikakohli29@gmail.com>
2014-08-08 12:40:44 +0530

    Styling and removing unused files

--- bugtracker/bugform/forms.py
+++ bugtracker/bugform/forms.py
@@ -2,11 +2,25 @@ from django.forms import ModelForm
 from bugform.models import BugModel, AdminModel
 import django_tables2 as tables
 from django_tables2.utils import A
+from django import forms
 
 class BugForm(ModelForm):
 	class Meta:
 		model = BugModel
 		fields = ['email', 'desc', 'date', 'loadtime', 'city', 'country', 'timezone', 'ip', 'netspeed', 'os', 'browser', 'bugstatus','bugpriority']
+		widgets = {
+			'browser':forms.HiddenInput(),
+			'os':forms.HiddenInput(),
+			'city':forms.HiddenInput(),
+			'loadtime':forms.HiddenInput(),
+			'date':forms.HiddenInput(),
+			'country':forms.HiddenInput(),
+			'bugstatus':forms.HiddenInput(),
+			'timezone':forms.HiddenInput(),
+			'ip':forms.HiddenInput(),
+			'netspeed':forms.HiddenInput(),
+			'desc':forms.Textarea
+			}
 		
 class AdminForm(ModelForm):
 	class Meta:
